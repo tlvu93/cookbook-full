f81c0482a1ba9fc919dc4bb1ffa54caefa756451
“tlvu93” <101603072+tlvu93@users.noreply.github.com>
2022-05-06 18:16:16 +0200

    Add Container Box

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,5 +1,4 @@
 import React, { ReactNode } from 'react';
-import Link from 'next/link';
 import Head from 'next/head';
 import Header from './Header';
 
@@ -9,7 +8,7 @@ type Props = {
 };
 
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
+  <div className='bg-gray-100 min-h-screen'>
     <Head>
       <title>{title}</title>
       <meta charSet='utf-8' />
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,16 +1,11 @@
-import Link from 'next/link';
+import Container from '../components/Container';
 import Layout from '../components/Layout';
 import Searchbar from '../components/Searchbar';
 
 const IndexPage = () => (
-  <Layout title='Home | Next.js + TypeScript Example'>
+  <Layout title='Home | Cookbook'>
     <Searchbar />
-    <h1 className='text-3xl font-bold underline'> Hello world! </h1>
-    <p>
-      <Link href='/about'>
-        <a>About</a>
-      </Link>
-    </p>
+    <Container> Hello </Container>
   </Layout>
 );
 
