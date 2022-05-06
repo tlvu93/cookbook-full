a3d55ec7f38c25179d7c8947e8b845a472b7a0ab
“tlvu93” <101603072+tlvu93@users.noreply.github.com>
2022-05-06 17:29:41 +0200

    Add Searchbar from old project

---
+++ frontend/components/Searchbar.tsx
@@ -0,0 +1,38 @@
+const Searchbar = () => {
+  return (
+    <div className='bg-slate-700 text-gray-100'>
+      <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
+        <div className='flex-1 flex items-center justify-center sm:items-stretch'>
+          <div className='flex-shrink-0 flex p-2 space-x-2 items-center'>
+            <button
+              type='button'
+              className='inline-flex item-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-500'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-6 w-auto'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+                />
+              </svg>
+            </button>
+            <input
+              type='text'
+              className='px-4 py-1 bg-slate-700 border-b-gray-100 border-b-2'
+              placeholder='Search by tags'
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Searchbar;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,15 +1,17 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from 'next/link';
+import Layout from '../components/Layout';
+import Searchbar from '../components/Searchbar';
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+  <Layout title='Home | Next.js + TypeScript Example'>
+    <Searchbar />
+    <h1 className='text-3xl font-bold underline'> Hello world! </h1>
     <p>
-      <Link href="/about">
+      <Link href='/about'>
         <a>About</a>
       </Link>
     </p>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
